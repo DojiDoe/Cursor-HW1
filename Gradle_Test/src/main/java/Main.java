8ff5c2c0d7f33f4e0b6a447f329680041307b9d2
Sasha <sasha.panchyshun@gmail.com>
2023-03-13 15:03:28 +0200

    Clean up ignored files

--- Gradle_Test/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.setAge(11);
-        person.setName("Oleg");
-        person.setWeight(35);
-        System.out.println(person.getName());
-    }
-}
--- Gradle_Test/src/main/java/Person.java
+++
@@ -1,9 +0,0 @@
-import lombok.*;
-
-@Getter
-@Setter
-public class Person {
-    int age;
-    String name;
-    int weight;
-}
