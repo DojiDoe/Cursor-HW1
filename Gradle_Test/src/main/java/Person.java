00f63379f769934a3469a05b70c98c54b6beb831
Sasha <sasha.panchyshun@gmail.com>
2023-03-13 15:05:56 +0200

    Clean up ignored files

---
+++ Gradle_Test/src/main/java/Person.java
@@ -0,0 +1,9 @@
+import lombok.*;
+
+@Getter
+@Setter
+public class Person {
+    int age;
+    String name;
+    int weight;
+}
---
+++ Maven_Project1/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setAge(20);
+        person.setName("Sasha");
+        person.setWeight(74);
+        System.out.println(person);
+    }
+
+}
